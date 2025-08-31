fbe276d4b3ecb9b079c94903a090524eef99ec25
Volodymyr Mazun <mebelmv1990@gmail.com>
2025-08-31 22:53:52 +0300

    correct file clientapi authprovider

--- components/AuthProvider/AuthProvider.tsx
+++ components/AuthProvider/AuthProvider.tsx
@@ -1,58 +1,77 @@
 
 // ----------КОМПОНЕНТ ВІДПОВІДАЄ ЗА ПЕРЕВІРКУ КОРИСТУВАЧА НА АВТОРИЗАЦІЮ----------
-// користувач є — зберігає його дані в глобальному стейті
-// користувача немає — очищає стейт і перенаправляє на /sign-in
+// зміна URL (pathname) перевіряє, чи є в користувача сесія
+// немає - очищає стан і відправляє на сторінку логіну
+// сесія є - завантажує дані користувача (getMe) і зберігає в глобальний стейт
+// дані отримати не вдалося - вихід з акаунту
 
 "use client";
 import { useEffect, useState } from "react";
 import { usePathname, useRouter } from "next/navigation";
-import { getSession, logoutUser } from "@/lib/api/clientApi";
+import { checkClientSession, getMe, logoutUser } from "@/lib/api/clientApi";
 import { useAuthStore } from "@/lib/store/authStore";
 
 type Props = { children: React.ReactNode };
 
-const isPrivatePath = (p: string) => p.startsWith("/notes") || p.startsWith("/profile");  //перевірка чи шлях приватний, для доступу потрібна авторизація
+const isPrivatePath = (p: string) => p.startsWith("/notes") || p.startsWith("/profile");    //функція, яка визначає, чи шлях вимагає авторизації
 
 export default function AuthProvider({ children }: Props) {
     const router = useRouter();
-    const pathname = usePathname();
-    const { setUser, clearAuth } = useAuthStore();
+    const pathname = usePathname();                                                         //поточний маршрут
+    const { setUser, clearAuth } = useAuthStore();                                          //працює з глобальним станом юзера
     const [checking, setChecking] = useState(true);
 
-    useEffect(() => {
+    useEffect(() => {                                                                       //асинхронна перевірка
         let active = true;
 
-        (async () => {
-            try {
-                const user = await getSession();
+    (async () => {
+        try {
+            const hasSession = await checkClientSession();
 
             if (!active) return;
 
-            if (user) {
-                setUser(user);
-            } else {
-                clearAuth();
-            if (isPrivatePath(pathname)) {
-                router.replace(`/sign-in?from=${pathname}`);
+            if (!hasSession) {
+                clearAuth();                                                                //очищає стан юзера (clearAuth)
+
+                if (isPrivatePath(pathname)) {
+                    router.replace(`/sign-in?from=${encodeURIComponent(pathname)}`);        //сторінка приватна, додає параметр from
                 return;
+                }
+            return;
             }
+
+            try {
+                const me = await getMe();                                                   //якщо є сесія, пробує отримати дані користувача
+                
+                if (!active) return;
+                setUser(me);
+            } catch {
+                clearAuth();
+                
+                if (isPrivatePath(pathname)) {
+                    try { await logoutUser(); } catch {}
+                        router.replace(`/sign-in?from=${encodeURIComponent(pathname)}`);     //Якщо це не вдалось, скидає стан і відправляє на логін
+                        return;
+                }
             }
         } catch {
             clearAuth();
+            
             if (isPrivatePath(pathname)) {
                 try { await logoutUser(); } catch {}
                 router.replace(`/sign-in?from=${encodeURIComponent(pathname)}`);
                 return;
             }
         } finally {
-        if (active) setChecking(false);
+            if (active) setChecking(false);
         }
     })();
 
-    return () => { active = false; };
-    }, [pathname, router, setUser, clearAuth]);
+        return () => { active = false; };
 
-    if (checking) return null; 
+    }, [pathname, clearAuth, router, setUser]);
+
+    if (checking) return null;
 
     return <>{children}</>;
 }
--- lib/api/clientApi.ts
+++ lib/api/clientApi.ts
@@ -2,71 +2,72 @@
 // --------ФАЙЛ, МІСТИТЬ ГОТОВІ ФУНКЦІЇ ДЛЯ АВТОРИЗАЦІЇ---------
 // функції для роботи з користувачем (getMe, updateUser)
 // функції для роботи з нотатками (fetchNotes, fetchNoteById, createNote, deleteNote)
+// ізольовує усі HTTP-запити в одному місці
 
-import api from "./api";
+import api from "./api";                                               //інстанс
 import { User } from "@/types/user";
-import { Note, CreateNotePayload } from "@/types/note";
+import { Note, CreateNotePayload } from "@/types/note";                //типізовані обєкти
 
-export interface NotesHttpResponse {
+export interface NotesHttpResponse {                                   //інтерфейс відповідь з бекенду
     notes: Note[];
-    totalPages: number;
+    totalPages: number;                                                //ксть сторнінок для пагінації
 }
 
-// -------АВТОРИЗАЦІЯ--------
-export async function registerUser(email: string, password: string): Promise<User> {
+// ----------ФУНКЦІЇ АВТОРИЗАЦІЇ----------
+export async function registerUser(email: string, password: string): Promise<User> {     //створює нового користувача
     const { data } = await api.post<User>("/auth/register", { email, password });
     return data;
 }
 
-export async function loginUser(email: string, password: string): Promise<User> {
+export async function loginUser(email: string, password: string): Promise<User> {        //логін користувача
     const { data } = await api.post<User>("/auth/login", { email, password });
     return data;
 }
 
-export async function logoutUser(): Promise<void> {
+export async function logoutUser(): Promise<void> {                                      //вихід з системи
     await api.post("/auth/logout");
 }
 
-export async function getSession(): Promise<User | null> {
+export async function getSession(): Promise<boolean> {                                   //перевірка чи активна сесія
     const { data } = await api.get<User | null>("/auth/session");
-    return data ?? null;
+    return Boolean(data);
 }
 
-export async function checkClientSession(): Promise<boolean> {
-    const user = await getSession();
-    return Boolean(user);
+export async function checkClientSession(): Promise<boolean> {                           //допоміжна, викликає getSession()
+    return getSession();
 }
 
-// ---------РОБОТА З ПРОФІЛЕМ-------
-export async function getMe(): Promise<User> {
+
+// ---------РОБОТА З ПРОФІЛЕМ-------  
+export async function getMe(): Promise<User> {                                           //отримує дані про користувача
     const { data } = await api.get<User>("/users/me");
     return data;
 }
-export async function updateUser(payload: { username?: string; email?: string }): Promise<User> {
-    const { data } = await api.patch<User>("/users/me", payload);
+
+export async function updateUser(payload: { username?: string; email?: string }): Promise<User> {  
+    const { data } = await api.patch<User>("/users/me", payload);                        //оновлює дані користувача
     return data;
 }
 
-// --------НОТАТКИ-------
+// --------РОБОТА З НОТАТКАМИ-------                                                     //отримати список нотаток
 export async function fetchNotes(params: { search?: string; page?: number; perPage?: number; tag?: string; }): Promise<NotesHttpResponse> {
     const { tag, ...rest } = params ?? {};
     const finalParams = { ...rest, ...(tag && tag !== "All" ? { tag } : {}) };
-
     const { data } = await api.get<NotesHttpResponse>("/notes", { params: finalParams });
     return data;
 }
 
-export async function fetchNoteById(id: string): Promise<Note> {
+export async function fetchNoteById(id: string): Promise<Note> {                         //отримує одну нотатку
     const { data } = await api.get<Note>(`/notes/${id}`);
     return data;
 }
 
-export async function createNote(note: CreateNotePayload): Promise<Note> {
+export async function createNote(note: CreateNotePayload): Promise<Note> {               //створює нову нотатку
     const { data } = await api.post<Note>("/notes", note);
     return data;
 }
 
-export async function deleteNote(id: string): Promise<Note> {
+export async function deleteNote(id: string): Promise<Note> {                            //видаляє нотатку
     const { data } = await api.delete<Note>(`/notes/${id}`);
     return data;
 }
